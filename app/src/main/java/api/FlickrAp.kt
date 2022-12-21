664fcff2201561fd7eccf9041a337a4fae5c5bf4
Gelizpa <gerliz2001@mail.ru>
2022-12-21 23:24:40 +0700

    Отображение результатов в RecyclerView

--- app/src/main/java/api/FlickrAp.kt
+++ app/src/main/java/api/FlickrAp.kt
@@ -8,13 +8,13 @@ import retrofit2.http.GET
  */
 
     interface FlickrApi {
-        @GET("services/rest/?method=flickr.interestingness.getList&api_key=e56347be8eeb6062f06669edaa6603b6&format=json&nojsoncallbac k=1&extras=url_s"
+        @GET(
 
-            /*"services/rest/?method=flickr.interestingness.getList" +
+            "services/rest/?method=flickr.interestingness.getList" +
                     "&api_key=e56347be8eeb6062f06669edaa6603b6" +
                     "&format=json" +
                     "&nojsoncallback=1" +// инструктирует Flickr убрать из ответа круглые скобки
-                    "&extras=url_s"//добавить URL-адрес мини-версии изображения, если таковая есть*/
+                    "&extras=url_s"//добавить URL-адрес мини-версии изображения, если таковая есть
         )
         fun fetchPhotos(): Call<FlickrResponse>//FlickrResponse для десериализации JSON-данных в ответе
     }// Объект Call представляет собой один веб-запрос, который вы можете выполнить.При выполнении вызова генерируется один соответствующий веб-отклик.
